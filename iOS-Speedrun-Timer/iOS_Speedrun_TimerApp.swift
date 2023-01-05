3b59f65ebf37d7d795176b3ef080aff1fef0b70c
Alex Eckardt <realalexeck@gmail.com>
2023-01-05 18:07:42 -0500

    Initial Commit

---
+++ iOS-Speedrun-Timer/iOS_Speedrun_TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_Speedrun_TimerApp.swift
+//  iOS-Speedrun-Timer
+//
+//  Created by Anita Eckardt on 2023-01-05.
+//
+
+import SwiftUI
+
+@main
+struct iOS_Speedrun_TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
