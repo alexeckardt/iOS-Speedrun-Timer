68ca03daa682e28172dd46cca71f844e9697ae9f
Alex Eckardt <realalexeck@gmail.com>
2023-01-06 20:35:16 -0500

    Added Start Run Timer

--- iOS-Speedrun-Timer/Page Views/MilestonePageView.swift
+++ iOS-Speedrun-Timer/Page Views/MilestonePageView.swift
@@ -12,21 +12,58 @@ struct MilestonePageView: View {
         
         VStack {
             
-            //Scrolling
-            ScrollView {
-                LazyVStack {
-                    ForEach(0 ... 20, id: \.self) { i in
+            ZStack(alignment: .bottomTrailing) {
+                
+                //Scrolling The Categories
+                ScrollView {
+                    LazyVStack {
                         
-                        //Show the Milestone
-                        MilestoneTabView(milestoneName: "1-" + String(i))
+                        //Go Over the Tabs
+                        ForEach(0 ... 20, id: \.self) { i in
+                            
+                            //Show the Milestone
+                            MilestoneTabView(milestoneName: "1-" + String(i))
+                            
+                        }
                         
+                        //History
+                        prevRunHistoryButton
                     }
                 }
+                
+                //New Run Button
+                NavigationLink {
+                    //
+                } label: {
+                    Image(systemName: "hourglass.tophalf.filled")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 45, height: 45)
+                        .padding(20)
+                }
+                .background(Color(.systemBlue))
+                .foregroundColor(Color(.systemBackground))
+                .clipShape(Circle())
+                .padding()
+                
             }
-            
             //
-            //Timer Info
-            TimerView()
+            //Timer
+            VStack {
+
+                //Divide Top
+                Divider()
+                
+                //Final
+                Text("PB")
+                    .font(.caption)
+                    .foregroundColor(Color(.systemGray))
+                    .padding(.top)
+                
+                TimeView()
+            
+                
+            }
         }
         
         //Navigation Header
@@ -36,6 +73,21 @@ struct MilestonePageView: View {
         //Toolbar, Edit Bar
         
     }
+}
+
+extension MilestonePageView {
+    
+    var prevRunHistoryButton : some View {
+        
+        NavigationLink {
+            //
+        } label: {
+            Text("Previous Run History")
+                .font(.caption)
+                .padding()
+        }
+    }
+    
 }
 
 struct MilestonePageView_Previews: PreviewProvider {
